de1463269afdf769a978e76a152d26f6df7e17ba
Valk Castellani <valkcastellani@gmail.com>
2022-10-17 19:48:06 -0300

    Projeto inicial do Spring Initialzr

---
+++ src/main/java/com/valkcastellani/con/CardapioOnLineApplication.java
@@ -0,0 +1,13 @@
+package com.valkcastellani.con;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioOnLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioOnLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valkcastellani/con/CardapioOnLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valkcastellani.con;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioOnLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
